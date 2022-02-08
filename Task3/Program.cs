e13cc957d446c5cfcdd6e65b84a96bab405585c6
Daniil <dtsikin@mail.ru>
2022-02-09 00:15:00 +0500

    Решили задачу 14

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿int GetRandomNumber ()
+{ 
+    int randomNumber = new Random().Next(0,10000);
+    return randomNumber;
+}
+int randomValue = GetRandomNumber();
+Console.WriteLine($"Дано число: {randomValue}");
+while (randomValue > 999)
+{
+    randomValue = randomValue / 10;
+}
+Console.WriteLine($"Необходимый диапазон {randomValue}");
+if (randomValue >= 100)
+{
+    Console.WriteLine($"Третье число {randomValue % 10}");
+}
+else
+{
+    Console.WriteLine($"Третьего числа нет");
+}
