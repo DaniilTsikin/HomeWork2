bc112b974f460dde06e71fed6c5d333bef840b96
Daniil <dtsikin@mail.ru>
2022-02-08 23:14:15 +0500

    Решена задача номер 10

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿int GetRandomNumber ()
+{ 
+    int randomNumber = new Random().Next(0,1000);
+    return randomNumber;
+}
+int randomValue = GetRandomNumber();
+Console.WriteLine($"Дано число: {randomValue}");
+
+int secondNumber = (randomValue % 100 - randomValue % 10) / 10;
+Console.WriteLine(secondNumber);
+if (randomValue >= 100)
+{
+    Console.WriteLine($"Второе число: {secondNumber}");
+}
