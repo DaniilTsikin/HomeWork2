07d4df95cb4961464711dd64f080a40360834ca3
Daniil <dtsikin@mail.ru>
2022-02-09 00:48:48 +0500

    Решили задачу 16

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿int GetRandomNumber ()
+{ 
+    int randomNumber = new Random().Next(1, 8);
+    return randomNumber;
+}
+int randomValue = GetRandomNumber();
+Console.WriteLine($"Дано число: {randomValue}");
+string[] weekDayName = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+if (randomValue < 6)
+{
+    Console.WriteLine($"{weekDayName[randomValue-1]} будний день");
+}
+else
+{
+    Console.WriteLine($"{weekDayName[randomValue-1]} выходной день");
+}
